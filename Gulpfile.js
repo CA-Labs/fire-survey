203edb796d5c1c12ef801b5d93324b93b76c8a80
Jacek Dominiak <jacek@sbdpartners.co.uk>
2015-01-13 14:18:12 +0100

    added Gulpfile

---
+++ Gulpfile.js
@@ -0,0 +1,54 @@
+/**
+ * Main gulpfile
+ * @author: Jacek Dominiak
+ * @copyright: Jacek Dominiak
+ * @created: 13/01/15
+ */
+
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify'),
+    traceur = require('gulp-traceur');
+
+// scopes definitions
+var src = [
+    'app/src/*.js',
+    'app/src/**/*.js'
+];
+
+var dist = [
+    'app/dist/*.js',
+    'app/dist/**/*.js'
+];
+
+// default task
+gulp.task('default', ['traceur', 'concat', 'uglify']);
+
+// watch task
+gulp.task('watch', function () {
+    gulp.watch(src, ['default']);
+});
+
+// traceur task
+gulp.task('traceur', function () {
+   gulp.src(src)
+    .pipe(traceur())
+    .pipe(gulp.dest('app/dist'));
+});
+
+// concat task
+gulp.task('concat', function () {
+    gulp.src(dist)
+     .pipe(concat('app.js'))
+     .pipe(gulp.dest('.'));
+});
+
+// uglify task
+gulp.task('uglify', function () {
+   gulp.src('app.js')
+    .pipe(uglify({mangle: false}))
+    .pipe(rename('app.min.js'));
+});
+
+
