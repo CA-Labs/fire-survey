452e85b2f33e2ae8a876367b64c8714f081f5475
Jacek Dominiak <jacek@jmbp.local>
2015-01-16 14:57:23 +0100

    added dynamic debug mode

--- app/src/controllers/MainController.js
+++ app/src/controllers/MainController.js
@@ -14,6 +14,12 @@ class MainController {
          * Inject configuration to be widely available
          */
         this.config = Configuration;
+
+        /**
+         * Flag to check if you should enable debug views or not as ones should only be available on localhost
+         * @type {boolean}
+         */
+        this.debug = (window.location.hostname == 'localhost');
     }
 
 }
